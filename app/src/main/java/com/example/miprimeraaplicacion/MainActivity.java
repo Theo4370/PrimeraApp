0e027d33ccb5b4560b29a8fc60eba172b9f271f9
Theo <theomusumeci@gmail.com>
2020-03-28 00:36:52 -0300

    Ejercicio 2

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -2,30 +2,48 @@ package com.example.miprimeraaplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button botonEnviar;
+    private Button botonCambiarColor;
+    private Button botonCambiarTexto;
+    private TextView textoPrincipal;
     private EditText nombre;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        linearLayout = findViewById(R.id.mainActivity_linearLayout);
+        botonCambiarColor = findViewById(R.id.mainActivity_botonColorEditText);
+        botonCambiarTexto = findViewById(R.id.mainActivity_botonContenidoTextView);
+        textoPrincipal = findViewById(R.id.mainActivity_textoPrincipal);
         nombre = findViewById(R.id.mainActivity_editorNombre);
         botonEnviar = findViewById(R.id.mainActivity_botonEnviar);
         botonEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String elNombreString = nombre.getText().toString();
-                //Toast.makeText(MainActivity.this, "Hola wachem", Toast.LENGTH_SHORT).show();
                 Toast.makeText(MainActivity.this, "Hola " + elNombreString + ", como andas?", Toast.LENGTH_SHORT).show();
+                linearLayout.setBackgroundColor(Color.RED);
+            }
+        });
+
+        botonCambiarColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textoPrincipal.setTextColor(Color.GREEN);
             }
         });
     }
